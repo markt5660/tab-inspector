b373b1e1b2a568eed72fe0392acc0c5b21f388e0
markt5660 <mark.thew@gmail.com>
2018-03-25 14:38:25 -0700

    Add Suspended Tabs retrieval

--- tabinspector.js
+++ tabinspector.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 /**
- * Retrieve all windows along with their tabs and pass them to the provided callback
+ * Retrieve all windows along with their tabs and pass them to the provided callback.
  * @param {*} callback 
  */
 function getTabsByWindow (callback) {
@@ -15,7 +15,23 @@ function getTabsByWindow (callback) {
 }
 
 /**
- * Process through each tab of each window and build the tree-view
+ * Retrieve "suspended" tabs stored as bookmarks and pass them to the provided callback.
+ * @param {*} callback 
+ */
+function getSuspendedTabs (callback) {
+  chrome.bookmarks.search({
+    title: 'Tabli Saved Windows'
+  }, (bookmarks) => {
+    if (bookmarks) {
+      chrome.bookmarks.getChildren(bookmarks[0].id, (bookmarks) => {
+        callback(bookmarks)
+      })
+    }
+  })
+}
+
+/**
+ * Process through each tab of each window and build the tree-view.
  * @param {*} windows 
  */
 var loadTabs2 = function (windows) {
@@ -43,9 +59,27 @@ var loadTabs2 = function (windows) {
 }
 
 /**
+ * Load the list of bookmarks into the "suspended tabs" tree.
+ * @param {*} bookmarks 
+ */
+var loadSuspended = function (bookmarks) {
+  var treeSuspend = createTree('suspend-tree', 'white')
+  if (bookmarks && bookmarks.length > 0) {
+    for (var i = 0; i < bookmarks.length; i++) {
+      var bookmark = bookmarks[i]
+      treeSuspend.createNode(bookmark.title, true, 'images/blue_key.png', null, null, null)
+    }
+  } else {
+    treeSuspend.createNode('Suspend empty', true, 'images/folder.png', null, null, null)
+  }
+  treeSuspend.drawTree()
+}
+
+/**
  * This extension loads the open tabs and displays them in a tree structure.
  */
 document.addEventListener('DOMContentLoaded', () => {
 //  getTabs(loadTabs1)
   getTabsByWindow(loadTabs2)
+  getSuspendedTabs(loadSuspended)
 })
